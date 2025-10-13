1fa42de6646cd99049c33b03d3d190afc488dc99
Davidjuhasz05 <juhasz.david.2005@gmail.com>
2025-10-13 15:38:54 +0200

    feat(tf): Add remote backend

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-mk8s-vm-state-bucket"
+    key            = "tf-mk8s-vm/terraform.tfstate"
+    region         = "eu-central-1"
+    use_lockfile   = true
+    encrypt        = true
+  }
+}
